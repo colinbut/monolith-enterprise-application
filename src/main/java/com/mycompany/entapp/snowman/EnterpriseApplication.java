885e1736db1f4e27507fbc425898e229918713b7
Colin But <colinbut@users.noreply.github.com>
2017-12-06 23:23:28 +0000

    setting up to run as a fat jar - for dev testing?

---
+++ src/main/java/com/mycompany/entapp/snowman/EnterpriseApplication.java
@@ -0,0 +1,13 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2017 Colin But. All rights reserved.
+ * |-------------------------------------------------
+ */
+package com.mycompany.entapp.snowman;
+
+public class EnterpriseApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Running as a Fat Jar");
+    }
+}
