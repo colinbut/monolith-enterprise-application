9b1b4747a44155bd8b7127d85da6318a08ad94b5
Colin But <colinbut@users.noreply.github.com>
2017-12-30 11:14:00 +0000

    user dao implementation using raw jdbc

--- src/main/java/com/mycompany/entapp/snowman/dao/impl/UserDaoImpl.java
+++ src/main/java/com/mycompany/entapp/snowman/dao/impl/UserDaoImpl.java
@@ -5,26 +5,65 @@
  */
 package com.mycompany.entapp.snowman.dao.impl;
 
+import com.mycompany.entapp.snowman.dao.AbstractJDBCDao;
 import com.mycompany.entapp.snowman.dao.UserDao;
 import com.mycompany.entapp.snowman.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
-import javax.annotation.PostConstruct;
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+// Use JDBCTemplate instead?
 @Repository
-public class UserDaoImpl implements UserDao {
+public class UserDaoImpl extends AbstractJDBCDao implements UserDao {
 
-    private Map<Integer, User> userMap = new HashMap<>();
+    private static final Logger LOG = LoggerFactory.getLogger(UserDaoImpl.class);
 
-    @PostConstruct
-    public void initialize() {
-        userMap.put(1, new User());
-    }
+    private static final String GET_USER_WITH_USERID_QUERY = "";
 
     @Override
     public User findUser(int userId) {
-        return userMap.get(userId);
+
+        Statement stmt = null;
+        Connection connection = null;
+
+        try {
+            setupDBDriver();
+            connection = getConnection();
+            stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(GET_USER_WITH_USERID_QUERY);
+
+            while(rs.next()) {
+                User user = new User();
+                user.setUserId(rs.getInt("id"));
+                user.setFirstname(rs.getString("firstname"));
+                user.setLastname(rs.getString("lastname"));
+                user.setUsername(rs.getString("username"));
+                user.setPassword(rs.getString("password"));
+                user.setEmail(rs.getString("email"));
+                return user;
+            }
+
+        } catch (SQLException e) {
+            LOG.error("{}", e);
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (SQLException e) {
+                LOG.error("{}", e);
+            }
+        }
+
+        return null;
     }
 }
