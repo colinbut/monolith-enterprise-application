85971c243dd301a3f2ae844df68b8faea1bafd6a
Colin But <colinbut@users.noreply.github.com>
2018-01-04 23:46:26 +0000

    test for ProjectResourceMapper

--- src/test/java/com/mycompany/entapp/snowman/endpoint/mappers/ProjectResourceMapperUTest.java
+++ src/test/java/com/mycompany/entapp/snowman/endpoint/mappers/ProjectResourceMapperUTest.java
@@ -8,6 +8,7 @@ package com.mycompany.entapp.snowman.endpoint.mappers;
 import com.mycompany.entapp.snowman.endpoint.resources.ProjectResource;
 import com.mycompany.entapp.snowman.model.Project;
 import junit.framework.TestCase;
+import org.joda.time.DateTime;
 import org.junit.Test;
 
 import java.util.Date;
@@ -16,23 +17,47 @@ public class ProjectResourceMapperUTest extends TestCase {
 
     @Test
     public void testMapToProject() throws Exception {
+        Date dateStarted = new DateTime(2018,1,1,0,0,0).toDate();
+        Date dateEnded = new DateTime(2018,5,1,0,0,0).toDate();
 
         ProjectResource projectResource = new ProjectResource();
+        projectResource.setProjectId(1);
+        projectResource.setTitle("Project Title");
+        projectResource.setDateStarted(dateStarted);
+        projectResource.setDateEnded(dateEnded);
 
         Project mappedProject = ProjectResourceMapper.mapToProject(projectResource);
 
         assertEquals(1, mappedProject.getId());
         assertEquals("Project Title", mappedProject.getProjectTitle());
-        assertEquals(new Date(), mappedProject.getDateStarted());
-        assertEquals(new Date(), mappedProject.getDateEnded());
+        assertEquals(dateStarted, mappedProject.getDateStarted());
+        assertEquals(dateEnded, mappedProject.getDateEnded());
     }
 
     @Test
     public void testMapToProjectResource() throws Exception {
+        int projectId = 1;
+        String projectTitle = "Project Title";
+        Date dateStarted = new DateTime(2018,1,1,0,0,0).toDate();
+        Date dateEnded = new DateTime(2018,5,1,0,0,0).toDate();
+
+        Project project = new Project();
+        project.setId(projectId);
+        project.setProjectTitle(projectTitle);
+        project.setDateStarted(dateStarted);
+        project.setDateEnded(dateEnded);
+
+        ProjectResource projectResource = ProjectResourceMapper.mapToProjectResource(project);
+
+        assertEquals(projectId, projectResource.getProjectId());
+        assertEquals(projectTitle, projectResource.getTitle());
+        assertEquals(dateStarted, projectResource.getDateStarted());
+        assertEquals(dateEnded, projectResource.getDateEnded());
     }
 
     @Test
     public void testMapToProjects() throws Exception {
+        fail("To be Implemented");
     }
 
 }
