35ea97e6cc25b52cbab47104c0c7775a7d749152
Colin But <colinbut@users.noreply.github.com>
2018-01-05 23:38:30 +0000

    moved more classes around and added first dummy repository

--- src/main/java/com/mycompany/entapp/snowman/domain/repository/ApplicationInfoRepository.java
+++ src/main/java/com/mycompany/entapp/snowman/domain/repository/ApplicationInfoRepository.java
@@ -3,7 +3,7 @@
  * | Copyright © 2018 Colin But. All rights reserved.
  * |-------------------------------------------------
  */
-package com.mycompany.entapp.snowman.infrastructure.db.repository;
+package com.mycompany.entapp.snowman.domain.repository;
 
 public interface ApplicationInfoRepository {
 }
