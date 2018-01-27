460012b3762629d20c87d5daf5cc1b372426c53e
Colin But <colinbut@users.noreply.github.com>
2018-01-27 23:17:04 +0000

    recognising ReportingSnapshotJob as a spring bean for component scanning

--- src/main/java/com/mycompany/entapp/snowman/infrastructure/scheduling/ReportingSnapshotJob.java
+++ src/main/java/com/mycompany/entapp/snowman/infrastructure/scheduling/ReportingSnapshotJob.java
@@ -10,14 +10,18 @@ import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ReportingSnapshotJob extends QuartzJobBean {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ReportingSnapshotTask.class);
 
     private ReportingSnapshotTask reportingSnapshotTask;
 
+    @Autowired
     public void setReportingSnapshotTask(ReportingSnapshotTask reportingSnapshotTask) {
         this.reportingSnapshotTask = reportingSnapshotTask;
     }
